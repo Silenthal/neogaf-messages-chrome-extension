b32c72d9559a253b375218c2ffbfa7ad9844d657
Andrew Rabon <andrewrabon@gmail.com>
2011-03-31 17:05:01 -0400

    Accidentally pushed the smilies extension instead of this one for some reason... oops.

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 # NeoGAF Smilies - Chrome Extension
-A just-for-fun extension for the NeoGAF message board adding back in the smilies functionality.
-[Chrome Web Store page here.](https://chrome.google.com/webstore/detail/gmhjjgpbnlipdlfddomlhljgffeemfgn)
+A just-for-fun extension for the NeoGAF message board that notifies you 
+using desktop ntofications whenever you get a new private message.
+[Chrome Web Store page here.](https://chrome.google.com/webstore/detail/dnpcgfdhjbipikkgnhlnceecljcheghp)
 
 # Known Bugs For Fixin'
-- Padding between the smilies disappears randomly.
-- After adding the [img] code the text cursor is not placed at the end of it.
+- If you don't read the PM within two minutes, another notification will pop up for the same PM.
 
 #Features to be Added
-- Add an options page.
-- Add custom smiley support.
+- Options page.
+- Optional sounds to be played when you get a new PM.
+- Auto-close the notification when it's clicked.
+- Fill out the notification with info about the sender and title of the PM.
+- Open the actual PM when clicked.
--- extension.js
+++ extension.js
@@ -1,18 +1,26 @@
 (function() {
-	var lol_smiley = 'http://i.imgur.com/KuGsj.gif',
-	grin_smiley = 'http://i.imgur.com/3AQmK.gif',
-	smilies = '<p class="smallfont">Smilies</p><img src="' + lol_smiley + '" id="lol_smiley" alt="Laughing" title=":lol">' + '<img src="' + grin_smiley + '" id="grin_smiley" alt="Grin" title=":D">';
+	var url, checkPMs, closeNotification, found = -1, notification, showing = false;
 	
-	$('#smiliebox').append(smilies);
-	$('#smiliebox').css('display', 'block');
+	url = 'http://www.neogaf.com/forum/private.php';
 	
-	$('#lol_smiley').click(function () {
-		$('textarea').val($('textarea').val() + '[img]' + lol_smiley + '[/img]');
-		$('textarea').focus();
-	});
+	// Basically what we're doing here is grabbing the whole PM list page via AJAX (we can use XHR since we're an extension,
+	// neat huh?) and then searching that massive string for a little part that tells us if we have a new PM or not. Then
+	// we display the notification, and set a timeout on that notification closing. Then we call ourself after 2 minutes.
+	checkPMs = function () {
+		$.get(url, function(data) {
+			found = data.search('<td class="alt1"><img src="images/neogaf/statusicon/pm_new.gif" alt="" border="0" /></td>');
+			if (found !== -1) {
+				notification = webkitNotifications.createHTMLNotification('notification.html');
+				notification.show();
+				window.setTimeout(closeNotification, 10000, notification);
+			}
+		}, 'html');
+		window.setTimeout(checkPMs, 120000);
+	};
 	
-	$('#grin_smiley').click(function () {
-		$('textarea').val($('textarea').val() + '[img]' + grin_smiley + '[/img]');
-		$('textarea').focus();
-	});
+	closeNotification = function (n) {
+		n.cancel();
+	};
+	
+	checkPMs();
 })();
