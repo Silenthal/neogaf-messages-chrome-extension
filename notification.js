e593f03d6a73a38c84983b5f4003dd1596153a9f
Silenthal Makram <silenthal.makram@gmail.com>
2013-05-10 09:17:40 -0400

    Formatting changes, code enclosure

--- notification.js
+++ notification.js
@@ -1,29 +1,27 @@
-var pmPage = "http://www.neogaf.com/forum/private.php?do=showpm&pmid=",
-allPage = "http://www.neogaf.com/forum/private.php",
-count = localStorage.msgCount,
-name = localStorage.name,
-title = localStorage.title,
-id = localStorage.id,
-clickAction = localStorage.toastClickAction,
-message = document.getElementById("msg"),
-senderSpan = document.getElementById("sender"),
-titleSpan = document.getElementById("title"),
-clickHref = document.getElementById("messageTarget"),
-msg;
-if (count == 1)
-{
-	msg = count + " unread message!";
-}
-else
-{
-	msg = count + " unread messages!";
-}
-senderSpan.innerHTML = name;
-titleSpan.innerHTML = title;
-message.innerHTML = msg;
-if (clickAction === "pm"){
-	clickHref.href = pmPage + id;
-}
-else if (clickAction === "all"){
-	clickHref.href = allPage;
-}
+(function() {
+	var pmPage = "http://www.neogaf.com/forum/private.php?do=showpm&pmid=",
+		allPage = "http://www.neogaf.com/forum/private.php",
+		count = localStorage.msgCount,
+		senderName = localStorage.name,
+		msgTitle = localStorage.title,
+		msgId = localStorage.id,
+		clickAction = localStorage.toastClickAction,
+		message = document.getElementById("msg"),
+		senderSpan = document.getElementById("sender"),
+		titleSpan = document.getElementById("title"),
+		clickHref = document.getElementById("messageTarget"),
+		msg;
+	if (count == 1) {
+		msg = count + " unread message!";
+	} else {
+		msg = count + " unread messages!";
+	}
+	senderSpan.innerHTML = senderName;
+	titleSpan.innerHTML = msgTitle;
+	message.innerHTML = msg;
+	if (clickAction === "pm") {
+		clickHref.href = pmPage + msgId;
+	} else if (clickAction === "all") {
+		clickHref.href = allPage;
+	}
+}());
--- options.js
+++ options.js
@@ -1,84 +1,83 @@
-function loadUpdateTime () {
-	var utVal = localStorage.updateTime;
-	if (!utVal) {
-		return;
-	}
-	var select = document.getElementById("updateTime");
-	for (var i = 0; i < select.children.length; i++) {
-		var child = select.children[i];
-		if (child.value == utVal) {
-			child.selected = "true";
-			break;
+(function() {
+	function loadUpdateTime() {
+		if ( !! localStorage.updateTime) {
+			var utVal = localStorage.updateTime,
+				select = document.getElementById("updateTime");
+			for (var i = 0; i < select.children.length; i++) {
+				var child = select.children[i];
+				if (child.value == utVal) {
+					child.selected = "true";
+					break;
+				}
+			}
 		}
 	}
-}
 
-function saveUpdateTime () {
-	var utSelect = document.getElementById("updateTime");
-	localStorage.updateTime = utSelect.children[utSelect.selectedIndex].value;
-}
-
-function loadToastClickAction () {
-	var tcaVal = localStorage.toastClickAction;
-	if (!tcaVal) {
-		return;
+	function saveUpdateTime() {
+		var utSelect = document.getElementById("updateTime");
+		localStorage.updateTime = utSelect.children[utSelect.selectedIndex].value;
 	}
-	var select = document.getElementById("toastClickAction");
-	for (var i = 0; i < select.children.length; i++) {
-		var child = select.children[i];
-		if (child.value == tcaVal) {
-			child.selected = "true";
-			break;
+
+	function loadToastClickAction() {
+		if ( !! localStorage.toastClickAction) {
+			var tcaVal = localStorage.toastClickAction,
+				select = document.getElementById("toastClickAction");
+			for (var i = 0; i < select.children.length; i++) {
+				var child = select.children[i];
+				if (child.value == tcaVal) {
+					child.selected = "true";
+					break;
+				}
+			}
+		}
+
+	}
+
+	function saveToastClickAction() {
+		var tuaSelect = document.getElementById("toastClickAction");
+		localStorage.toastClickAction = tuaSelect.children[tuaSelect.selectedIndex].value;
+	}
+
+	function loadNagAction() {
+		var naVal = localStorage.nagAction || "nagonce",
+			nagonceRadio = document.getElementById("notify_nagonce"),
+			nagRadio = document.getElementById("notify_nag");
+		if (naVal == nagRadio.value) {
+			nagRadio.checked = true;
+		} else if (naVal == nagonceRadio.value) {
+			nagonceRadio.checked = true;
 		}
 	}
-}
 
-function saveToastClickAction () {
-	var tuaSelect = document.getElementById("toastClickAction");
-	localStorage.toastClickAction = tuaSelect.children[tuaSelect.selectedIndex].value;
-}
-
-function loadNagAction () {
-	var naVal = localStorage.nagAction,
-	nagonceRadio = document.getElementById("notify_nagonce"),
-	nagRadio = document.getElementById("notify_nag");
-	if (naVal == nagRadio.value){
-		nagRadio.checked = true;
+	function saveNagAction() {
+		var nagonceRadio = document.getElementById("notify_nagonce"),
+			nagRadio = document.getElementById("notify_nag");
+		if (nagRadio.checked) {
+			localStorage.nagAction = nagRadio.value;
+		} else if (nagonceRadio.checked) {
+			localStorage.nagAction = nagonceRadio.value;
+		}
 	}
-	else if (naVal == nagonceRadio.value){
-		nagonceRadio.checked = true;
-	}
-}
 
-function saveNagAction () {
-	var nagonceRadio = document.getElementById("notify_nagonce"),
-	nagRadio = document.getElementById("notify_nag");
-	if (nagRadio.checked){
-		localStorage.nagAction = nagRadio.value;
+	function loadOptions() {
+		loadUpdateTime();
+		loadToastClickAction();
+		loadNagAction();
 	}
-	else if (nagonceRadio.checked){
-		localStorage.nagAction = nagonceRadio.value;
-	}
-}
 
-function loadOptions () {
-	loadUpdateTime();
-	loadToastClickAction();
-	loadNagAction();
-}
-
-$(document).ready(function () {
-	loadOptions();
-	$("#updateTime").change(function () {
-		saveUpdateTime();
+	$(document).ready(function() {
+		loadOptions();
+		$("#updateTime").change(function() {
+			saveUpdateTime();
+		});
+		$("#toastClickAction").change(function() {
+			saveToastClickAction();
+		});
+		$("#notify_nag").change(function() {
+			saveNagAction();
+		});
+		$("#notify_nagonce").change(function() {
+			saveNagAction();
+		});
 	});
-	$("#toastClickAction").change(function () {
-		saveToastClickAction();
-	});
-	$("#notify_nag").change(function () {
-		saveNagAction();
-	});
-	$("#notify_nagonce").change(function () {
-		saveNagAction();
-	});
-});
+}());
